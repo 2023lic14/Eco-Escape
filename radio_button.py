43e58bed37b36899078f9ca71679473585c42f7c
Claire <2023lic14@gmail.com>
2024-02-04 01:46:40 -0500

    added radio textbox

--- radio_button.py
+++ radio_button.py
@@ -1,16 +1,18 @@
 # radio_button.py
 
 from kivy.uix.image import Image
-from kivy.uix.behaviors import ButtonBehavior
 from kivy.properties import BooleanProperty
+from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
 
 class RadioImageButton(Image):
     def __init__(self, original_size=(200, 200), original_pos=(400, 400), **kwargs):
         super(RadioImageButton, self).__init__(**kwargs)
+
         self.original_image_normal = 'radiostart.png'
         self.new_image_normal = 'radio.png'
         self.is_unlocked = BooleanProperty(False)
+        self.click_counter = 0  # Track the number of clicks
 
         self.original_size = original_size
         self.original_pos = original_pos
@@ -19,6 +21,12 @@ class RadioImageButton(Image):
         self.pos = self.original_pos
         self.size = self.original_size
 
+        # Create a separate TextInput for the textbox
+        self.textbox = TextInput(text="Press the correct keys on your keyboard.", readonly=True,
+                                 size_hint=(None, None), size=(300, 60), pos=(-1000, -1000))
+        self.add_widget(self.textbox)  # Add the textbox initially
+        self.textbox.opacity = 0  # Hide the textbox initially
+
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.toggle_unlock_mode()
@@ -26,18 +34,22 @@ class RadioImageButton(Image):
         return super(RadioImageButton, self).on_touch_down(touch)
 
     def toggle_unlock_mode(self):
-        if self.is_unlocked:
-            self.source = self.original_image_normal
-            self.size_hint = (None, None)
-            self.size = self.original_size
-            self.pos = self.original_pos
-        else:
+        self.click_counter += 1
+
+        if self.click_counter % 2 == 1:  # Show the textbox on odd clicks (third, fifth, etc.)
             self.source = self.new_image_normal
             new_size = (4000, 4000)  # Adjust the values for a larger size
             self.size_hint = (None, None)
             self.size = new_size
             self.pos = ((Window.width - new_size[0]) / 2, (Window.height - new_size[1]) / 2)
-
-        self.is_unlocked = not self.is_unlocked
-
-
+            self.textbox.pos = (Window.width / 2 - self.textbox.width / 2,
+                                Window.height / 2 - self.textbox.height / 2 - 200)  # Move 200 pixels lower
+            self.textbox.opacity = 1
+            self.is_unlocked = True
+        else:  # Hide the textbox on even clicks (fourth, sixth, etc.)
+            self.source = self.original_image_normal
+            self.size_hint = (None, None)
+            self.size = self.original_size
+            self.pos = self.original_pos
+            self.textbox.opacity = 0
+            self.is_unlocked = False
